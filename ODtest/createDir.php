356650db08d14d0d9f123f4e4c843a9652cdd73a
mid2sick <dotty12313@yahoo.com>
2022-05-25 00:19:03 +0800

    reconstruct table, date: 0522

--- ODtest/createDir.php
+++ ODtest/createDir.php
@@ -20,11 +20,9 @@
         $query = "INSERT INTO `directory`(`dirName`, `username`,`metadata`) VALUES ('$dirName','$username','[]')";
         mysqli_query($db, $query);
         
-        // get the new directory's id
-        $query = "SELECT directory.dirID FROM `directory` INNER JOIN (SELECT username, MAX(dirID) AS most_recent_dirID FROM `directory` GROUP BY username) tmpTable ON directory.username = '$username' AND directory.dirID = most_recent_dirID WHERE 1";
-        $result = mysqli_query($db, $query);
-        $curID = mysqli_fetch_assoc($result);
-        $curID = $curID["dirID"];
+        // this query can acquire the largest dirID from a user
+        // $query = "SELECT directory.dirID FROM `directory` INNER JOIN (SELECT username, MAX(dirID) AS most_recent_dirID FROM `directory` GROUP BY username) tmpTable ON directory.username = '$username' AND directory.dirID = most_recent_dirID WHERE 1";
+        
 
         // return if the edit successed                
         $_SESSION['uploadSuccess'] = "Directory successfully created.<br>";
